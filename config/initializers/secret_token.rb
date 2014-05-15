898e349f607e97c68a0a8b0e1033d949b4d931fa
Judobob <bobalonfife@gmail.com>
2014-05-15 14:42:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BobRails::Application.config.secret_key_base = 'eec8dd5d15fd27518c54e306e03f22cabff81418e757940ce3ebdc85e264b6e2ad26c0cff41f6c33df1924ca35c6f71668fc46f74a790c36ed2557702c387b53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BobRails::Application.config.session_store :cookie_store, key: '_bobRails_session'
